5454151fc01e0717fbb2cc6eddc676873d192cee
sgbourque <sb_coyote@hotmail.com>
2015-10-02 00:15:36 -0400

    Preparing Clifford Algebra :  + bit_traits  + static_for_each  + exterior algebra traits (alternating product traits, reversion/adjoint/clfford dual/hodge dual traits)

---
+++ src/SBLib/Algorithms/static_for_each.h
@@ -0,0 +1,20 @@
+#pragma once
+
+template<size_t begin, size_t end, class get_helper, class increment_helper>
+struct static_for_each
+{
+	template<class fct_type>
+	static void iterate(fct_type fct)
+	{
+		fct(get_helper::get_helper<begin>::value, begin);
+		static_for_each<increment_helper::increment_helper<begin>::value, end, get_helper, increment_helper>::iterate(fct);
+	}
+};
+template<size_t end, class get_helper, class increment_helper>
+struct static_for_each<end, end, get_helper, increment_helper>
+{
+	template<class fct_type>
+	static void iterate(fct_type fct)
+	{
+	}
+};
---
+++ src/SBLib/Traits/bit_traits.h
@@ -0,0 +1,177 @@
+#pragma once
+
+template<size_t bit_set>
+struct bit_traits
+{
+private:
+	enum : size_t
+	{
+		first_bit        = (((bit_set - 1) & ~bit_set) + 1),
+		remaining_bits   = (bit_set & ~first_bit),
+	};
+public:
+	enum : size_t
+	{
+		value            = bit_set,
+		population_count = 1 + bit_traits<remaining_bits>::population_count,
+	};
+
+	template<size_t index>
+	struct set_bit
+	{
+		enum : size_t
+		{
+			value = bit_traits<remaining_bits>::set_bit<index - 1>::value,
+		};
+	};
+	template<>
+	struct set_bit<0>
+	{
+		enum : size_t
+		{
+			value = first_bit,
+		};
+	};
+
+	template<size_t bit>
+	struct bit_index
+	{
+		enum : size_t
+		{
+			value = 1 + bit_traits<remaining_bits>::bit_index<bit>::value,
+		};
+	};
+	template<>
+	struct bit_index<first_bit>
+	{
+		enum : size_t
+		{
+			value = 0,
+		};
+	};
+
+	template<size_t index>
+	static constexpr size_t get_set_bit()
+	{
+		return set_bit<index>::value;
+	}
+	template<size_t bit>
+	static constexpr size_t get_bit_index()
+	{
+		return bit_index<bit>::value;
+	}
+};
+template<>
+struct bit_traits<0>
+{
+public:
+	enum : size_t
+	{
+		value = 0,
+		population_count = 0,
+	};
+
+	template<size_t>
+	struct set_bit
+	{
+		enum : size_t
+		{
+			value = 0,
+		};
+	};
+	template<size_t bit>
+	struct bit_index
+	{
+	};
+
+	template<size_t index>
+	static constexpr size_t get_set_bit()
+	{
+		return set_bit<index>::value;
+	}
+	template<size_t bit>
+	static constexpr size_t get_bit_index()
+	{
+		return bit_index<bit>::value;
+	}
+};
+
+template<class traits>
+struct get_set_bit_helper
+{
+	template<size_t index>
+	struct get_helper
+	{
+		enum : size_t
+		{
+			value = traits::set_bit<index>::value,
+		};
+	};
+
+	template<size_t index>
+	static constexpr size_t get()
+	{
+		return get_helper<index>::value;
+	}
+};
+template<class traits>
+struct increment_bit_index_helper
+{
+	template<size_t index>
+	struct increment_helper
+	{
+		enum : size_t
+		{
+			value = index + 1,
+		};
+	};
+
+	template<size_t index>
+	static constexpr size_t increment()
+	{
+		return increment_helper<index>::value;
+	}
+};
+
+template<class traits>
+struct get_bit_index_helper
+{
+	template<size_t bit>
+	struct get_helper
+	{
+		enum : size_t
+		{
+			value = traits::bit_index<bit>::value,
+		};
+	};
+
+	template<size_t bit>
+	static constexpr size_t get()
+	{
+		return get_helper<bit>::value;
+	}
+};
+template<class traits>
+struct next_set_bit_helper
+{
+	template<size_t bit>
+	struct increment_helper
+	{
+	private:
+		enum : size_t
+		{
+			next_index = traits::bit_index<bit>::value + 1,
+		};
+	public:
+		enum : size_t
+		{
+			value = traits::set_bit<next_index>::value,
+		};
+	};
+
+	template<size_t bit>
+	static constexpr size_t increment()
+	{
+		return increment_helper<bit>::value;
+	}
+};
